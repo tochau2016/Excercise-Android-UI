9a45c50f85243e9e61d9cd61ac4aec4fb814c09d
Chau Nguyen <tochau2016@gmail.com>
2020-07-08 00:06:37 +0700

    initial commit

---
+++ app/src/main/java/com/baitap/english/ActivityRelativeLayout.java
@@ -0,0 +1,14 @@
+package com.baitap.english;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityRelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+}
